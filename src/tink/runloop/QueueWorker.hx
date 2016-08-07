f9bd8357dd0cb6ea157111f99f4ef9164a46363b
Russ Panula <dfadev@users.noreply.github.com>
2016-08-07 14:10:42 -0500

    simplify

--- src/tink/runloop/QueueWorker.hx
+++ src/tink/runloop/QueueWorker.hx
@@ -50,7 +50,7 @@ class QueueWorker implements Worker {
   }
   
   public function kill()
-    asap(function() { tasks = null; } );
+    asap(function() tasks = null);
   
   function error(e:Error, t:Task) 
     owner.asap(function () owner.onError(e, t, this, haxe.CallStack.exceptionStack()));
